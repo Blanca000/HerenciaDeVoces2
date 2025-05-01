be973c9aa5ba28300ab7ebd4932ec50c010ece93
Eduardo Sánchez <sahe000610@gs.utm.mx>
2025-04-30 21:01:44 -0600

    Ultimo pase de lista(actualizaciones)

--- app/src/main/java/com/example/herenciadevoces/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/herenciadevoces/ui/main/MainActivity.kt
@@ -12,10 +12,11 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         installSplashScreen()
         enableEdgeToEdge()
         setContent {
-            HerenciaDeVocesTheme {
+            HerenciaDeVocesTheme(darkTheme = false, dynamicColor = false) {
                 AppNavigation()
             }
         }
--- app/src/main/java/com/example/herenciadevoces/ui/theme/Theme.kt
+++ app/src/main/java/com/example/herenciadevoces/ui/theme/Theme.kt
@@ -27,18 +27,10 @@ fun HerenciaDeVocesTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = LightColorScheme
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = LightColorScheme,
         typography = Typography,
         content = content
     )
